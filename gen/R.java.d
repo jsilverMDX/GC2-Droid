ab50a4fcf711779f150caf0d3ddf3475480c3f91
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2012-12-05 03:12:02 -0800

    check it all in.

--- gen/R.java.d
+++ gen/R.java.d
@@ -5,8 +5,4 @@
 /Users/jsilverman2/mdks/GC2-Droid/res/drawable-mdpi/ic_launcher.png \
 /Users/jsilverman2/mdks/GC2-Droid/res/layout/get_ruboto_core.xml \
 /Users/jsilverman2/mdks/GC2-Droid/res/values/strings.xml \
-/Users/jsilverman2/mdks/GC2-Droid/bin/res/drawable/get_ruboto_core.png \
-/Users/jsilverman2/mdks/GC2-Droid/bin/res/drawable-hdpi/ic_launcher.png \
-/Users/jsilverman2/mdks/GC2-Droid/bin/res/drawable-ldpi/ic_launcher.png \
-/Users/jsilverman2/mdks/GC2-Droid/bin/res/drawable-mdpi/ic_launcher.png \
 /Users/jsilverman2/mdks/GC2-Droid/bin/AndroidManifest.xml \
